c6720df26b7d32377046476c88c057780f2be89a
MemeLady <alex.kolpakova2001@gmail.com>
2022-03-06 22:09:32 +0700

    lab2

---
+++ Lab2.2/Lab2.2/Controllers/HomeController.cs
@@ -0,0 +1,124 @@
+﻿using Lab2._2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace Lab2._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SingleAction()
+        {
+            return View();
+        }
+
+        public IActionResult SeparateAction()
+        {
+            return View();
+        }
+
+        public IActionResult BindingParametrs()
+        {
+            return View();
+        }
+
+        public IActionResult BindingSeparate()
+        {
+            return View();
+        }
+
+        public double Calculate(double first, double second, string operation)
+        {
+            double result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+                case "*":
+                    result = first * second;
+                    break;
+                case "/":
+                    result = first / second;            
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            return result;
+        }
+
+        public IActionResult SingleAct()
+        {
+            if (Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase))
+            {
+                double first = Convert.ToInt32(this.Request.Form["first"]);
+                double second = Convert.ToInt32(this.Request.Form["second"]);
+                string operations = this.Request.Form["operations"];
+
+                ViewBag.firstValue = first;
+                ViewBag.secondValue = second;
+                ViewBag.operations = operations;
+                ViewBag.result = Calculate(first, second, operations);
+
+            }
+
+            return View("Result");
+        }
+
+        [HttpPost, ActionName("SeparateAct")]
+        public IActionResult SeparateAct()
+        {
+            double first = Convert.ToInt32(this.Request.Form["first"]);
+            double second = Convert.ToInt32(this.Request.Form["second"]);
+            string operations = this.Request.Form["operations"];
+
+            ViewBag.firstValue = first;
+            ViewBag.secondValue = second;
+            ViewBag.operations = operations;
+            ViewBag.result = Calculate(first, second, operations);
+
+            return View("Result");
+        }
+
+        [HttpPost]
+        public IActionResult BindingPar(int first, int second, string operations)
+        {
+            ViewBag.firstValue = first;
+            ViewBag.secondValue = second;
+            ViewBag.operations = operations;
+            ViewBag.result = Calculate(first, second, operations);
+
+            return View("Result");
+        }
+
+        [HttpPost]
+        public IActionResult BindingSep(Calc calc)
+        {
+            ViewBag.firstValue = calc.first;
+            ViewBag.secondValue = calc.second;
+            ViewBag.operations = calc.operations;
+            ViewBag.result = calc.Calculate();
+            Console.WriteLine(calc.first + calc.second + calc.operations + calc.Calculate() + "*********************");
+            return View("Result");
+        }
+    }
+}
---
+++ Lab2.2/Lab2.2/Models/Calc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2._2.Models
+{
+    public class Calc
+    {
+        public double first { get; set; }
+        public double second { get; set; }
+        public string operations { get; set; }
+
+        public Calc() { }
+
+        public Calc(double first_, double second_, string operation)
+        {
+            first = first_;
+            second = second_;
+            operations = operation;
+        }
+
+        public double Calculate()
+        {
+            double result;
+
+            switch (operations)
+            {
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+                case "*":
+                    result = first * second;
+                    break;
+                case "/":
+                    result = first / second;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
